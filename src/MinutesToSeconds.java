0aacebb3e63292d048546dff0a1021d4400938be
THVstars <kcarolinasanchez@gmail.com>
2021-12-16 13:18:17 -0500

    Added MinutesToSeconds and ReturnSomethingToMe.

---
+++ src/MinutesToSeconds.java
@@ -0,0 +1,8 @@
+public class MinutesToSeconds {
+    public static void main(String[] args) {
+        convert(60);
+    }
+    public static int convert(int minutes) {
+        return minutes*60;
+    }
+}
---
+++ src/ReturnSomethingToMe.java
@@ -0,0 +1,8 @@
+public class ReturnSomethingToMe {
+    public static void main(String[] args) {
+
+    }
+    public static String giveMeSomething(String a) {
+        return "something " + a;
+    }
+}
