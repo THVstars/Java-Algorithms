cbf8836a1235a526130f71ca97dcebfd60c84027
THVstars <kcarolinasanchez@gmail.com>
2021-12-16 13:23:10 -0500

    TEST.

--- src/ReturnSomethingToMe.java
+++ src/ReturnSomethingToMe.java
@@ -1,7 +1,7 @@
 public class ReturnSomethingToMe {
     public static void main(String[] args) {
 
-    }
+    } // TEST
     public static String giveMeSomething(String a) {
         return "something " + a;
     }
