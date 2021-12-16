476d694c695399750f09fdef34f3fb025f752942
THVstars <kcarolinasanchez@gmail.com>
2021-12-16 12:38:16 -0500

    TESTING..

--- src/GetFirstElement.java
+++ src/GetFirstElement.java
@@ -1,6 +1,6 @@
 public class GetFirstElement {
     public static void main(String[] args) {
-        System.out.println(getFirstValue(new int[] {1,2,3,4,5}));
+        System.out.println(getFirstValue(new int[] {1,2,3,4,5})); // TEST
     } // void because it's the main method, where we want to return the values of other methods. in this case, it'll return the value of the getFirstValue method only.
 
     public static int getFirstValue(int[] arr) {
