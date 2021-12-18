23c1ea7a97c60c3e51d723811ddb729720e93ec3
THVstars <kcarolinasanchez@gmail.com>
2021-12-17 19:01:15 -0500

    Added AddUpNumbers.

---
+++ src/AddUpNumbers.java
@@ -0,0 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class AddUpNumbers {
+    public static void main(String[] args) {
+        System.out.println(addUp(7));
+    }
+
+    public static int addUp(int num) {
+        int added = 0;
+        for (int i = 0; i < num; i++) {
+            added += i;
+        } return added + num;
+    }
+}
