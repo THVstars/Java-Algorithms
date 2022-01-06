b10d0f7de28b554e5af55313686c0cc3d1c9c93b
THVstars <kcarolinasanchez@gmail.com>
2022-01-06 14:25:15 -0500

    Updated MatchstickHouses.

--- src/MatchstickHouses.java
+++ src/MatchstickHouses.java
@@ -4,7 +4,9 @@ public class MatchstickHouses {
     }
 
     public static int matchHouses(int step) {
-        int matches = step*6 - (step-1)*1;
-        return matches;
+        int matches = 0;
+        if (step > 0) {
+            matches = step*6 - (step - 1); // can also do step*5 + 1
+        } return matches;
     }
 }
