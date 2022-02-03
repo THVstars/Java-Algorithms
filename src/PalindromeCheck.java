2bd03cc5f4218a8e84b0a22cac1809a1db303a5a
THVstars <kcarolinasanchez@gmail.com>
2022-02-03 12:31:52 -0500

    Updated the PalindromeCheck class.

--- src/PalindromeCheck.java
+++ src/PalindromeCheck.java
@@ -1,29 +1,20 @@
 public class PalindromeCheck {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Racecar"));
+        System.out.println(isPalindrome("Race_car"));
     }
 
     public static boolean isPalindrome(String s) {
-        String[] checkPalindrome = s.split("[, :]+");
+        String[] checkPalindrome = s.split("[\\W_]+");
         String forwards = "";
         String backwards = "";
-        String backwardsReversed = "";
 
         for (int i = 0; i < checkPalindrome.length; i++) {
             forwards += checkPalindrome[i];
-            //System.out.println(forwards);
         }
         for (int i = forwards.length() - 1; i >= 0; i--) {
             backwards += forwards.charAt(i);
-            //System.out.println(backwards);
-        }
-        for (int i = backwards.length() - 1; i >= 0; i--) {
-            backwardsReversed += backwards.charAt(i);
-            //System.out.println(backwardsReversed);
         }
 
-        System.out.println(forwards);
-        System.out.println(backwardsReversed);
-        return forwards.equalsIgnoreCase(backwardsReversed); // instead of having two .toLowerCase we can have one .equalsIgnoreCase
+        return forwards.equalsIgnoreCase(backwards); // instead of having two .toLowerCase we can have one .equalsIgnoreCase
     }
 }
